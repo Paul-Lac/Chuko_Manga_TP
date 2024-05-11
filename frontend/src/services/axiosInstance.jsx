82691d484eea6e190d43d52c92d4c07bc44a4572
Paul-Lac <plnlacroix@gmail.com>
2024-05-11 23:01:24 +0200

    minor changes for documentation purposes

--- frontend/src/services/axiosInstance.jsx
+++ frontend/src/services/axiosInstance.jsx
@@ -36,36 +36,3 @@ axiosInstance.interceptors.response.use(
 );
 
 export default axiosInstance;
-// import axios from "axios";
-
-// // Create an axios instance
-// const axiosInstance = axios.create({
-//   baseURL: "http://localhost:3310/api",
-// });
-
-// // Add a response interceptor
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     const originalRequest = error.config;
-
-//     if (error.response.status === 401 && !originalRequest.retry) {
-//       originalRequest.retry = true;
-
-//       try {
-//         await axios.post("http://localhost:3310/api/refresh", {
-//           withCredentials: true,
-//         });
-//         return axiosInstance(originalRequest);
-//       } catch (refreshError) {
-//         console.error("Token refresh failed:", refreshError);
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default axiosInstance;
