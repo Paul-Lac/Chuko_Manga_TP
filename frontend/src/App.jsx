358846f57a1852fc352f5d14c765759fa2923706
Paul-Lac <plnlacroix@gmail.com>
2024-05-17 12:40:36 +0200

    Add Scroll to top component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import HeaderNav from "./components/HeaderNav";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
 import "./style/global.css";
 import "./style/variables.css";
@@ -9,6 +10,7 @@ import "./style/variables.css";
 function App() {
   return (
     <UserProvider>
+      <ScrollToTop />
       <div className="mainContent">
         <HeaderNav />
         <Outlet />
